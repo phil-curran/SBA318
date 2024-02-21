816edb44c73c0d1d97fcead22d285c7c744a6293
Phil Curran <pecurran@outlook.com>
2024-02-21 14:54:18 -0800

    moved files around to simplify structure; no separate backend dir now; also added data folder and dummy data for the three types of data being used.

---
+++ backend/data/tasks.js
@@ -0,0 +1,25 @@
+const tasks = [
+  {
+    id: 1,
+    userId: 1,
+    title: "est et quae odit qui non",
+    content:
+      "similique esse doloribus nihil accusamus\nomnis dolorem fuga consequuntur reprehenderit fugit recusandae temporibus\nperspiciatis cum ut laudantium\nomnis aut molestiae vel vero",
+  },
+  {
+    id: 2,
+    userId: 1,
+    title: "quasi id et eos tenetur aut quo autem",
+    content:
+      "eum sed dolores ipsam sint possimus debitis occaecati\ndebitis qui qui et\nut placeat enim earum aut odit facilis\nconsequatur suscipit necessitatibus rerum sed inventore temporibus consequatur",
+  },
+  {
+    id: 3,
+    userId: 1,
+    title: "delectus ullam et corporis nulla voluptas sequi",
+    content:
+      "non et quaerat ex quae ad maiores\nmaiores recusandae totam aut blanditiis mollitia quas illo\nut voluptatibus voluptatem\nsimilique nostrum eum",
+  },
+];
+
+module.exports = tasks;
