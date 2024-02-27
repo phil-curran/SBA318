a4353dad00a98863521d4e9180773f60061e8f46
Phil Curran <pecurran@outlook.com>
2024-02-27 14:59:21 -0800

    updated tasks.js route; didnt like nested routes after all.

--- routes/tasks.js
+++ routes/tasks.js
@@ -16,47 +16,46 @@ router.use(routeLogger);
 router.use(express.json());
 
 // ROUTES
+// create task
+router.route("/").post((req, res, next) => {
+  let id = tasks.length + 1;
+  let userId = req.body.userId;
+  let title = req.body.title;
+  let content = req.body.content;
+  let dateAdded = new Date();
+  let temp = { id, userId, title, content, dateAdded };
+  tasks.push(temp);
+  res.render("index", { tasks });
+});
+
+// read: get all tasks
 router
   .route("/")
-  // create
-  .post((req, res, next) => {
-    let id = tasks.length + 1;
-    let userId = req.body.userId;
-    let title = req.body.title;
-    let content = req.body.content;
-    let dateAdded = new Date();
-    let temp = { id, userId, title, content, dateAdded };
-    tasks.push(temp);
-    res.render("index", { tasks });
-  })
   // read
   .get((req, res, next) => {
     res.json(tasks);
-  })
-  // update
-  .put((req, res, next) => {
-    res.send(
-      `Method: ${req.method}\nStatus: ${res.statusCode}\nPath: ${req.baseUrl}`
-    );
   });
 
-router
-  .route("/:id")
-  // get specific task
-  .get((req, res, next) => {
-    const task = tasks.find((task) => task.id == req.params.id);
-    !task
-      ? (console.log("Task not found"),
-        res.status(404).json({ error: "Task not found" }))
-      : // : res.json(task);
-        res.render("task-detail", { task });
-  })
-  // delete
-  .delete((req, res, next) => {
-    tasks = tasks.filter((task) => req.params.id != task.id);
-    // res.json(tasks);
-    res.redirect("index", { tasks });
-    // res.render("index", { tasks });
-  });
+// read: get specific task
+router.route("/:id").get((req, res, next) => {
+  const task = tasks.find((task) => task.id == req.params.id);
+  !task
+    ? (console.log("Task not found"),
+      res.status(404).json({ error: "Task not found" }))
+    : res.render("task-detail", { task });
+});
+
+// update task
+router.route("/").put((req, res, next) => {
+  res.send(
+    `Method: ${req.method}\nStatus: ${res.statusCode}\nPath: ${req.baseUrl}`
+  );
+});
+
+// delete task
+router.route("/:id").delete((req, res, next) => {
+  tasks = tasks.filter((task) => req.params.id != task.id);
+  res.redirect("index", { tasks });
+});
 
 module.exports = router;
