9c317bc72c1ee830296f2c437def6c2d9eaf8128
Phil Curran <pecurran@outlook.com>
2024-02-27 15:05:53 -0800

    also updated other route files to remove nested routes.

--- routes/comments.js
+++ routes/comments.js
@@ -12,6 +12,21 @@ router.use(routeLogger);
 router.use(express.json());
 
 // ROUTES
+// create comment
+router.route("/").post((req, res, next) => {
+  // let { userId, taskParent, commentParent, title, content } = req.body;
+  let temp = { ...req.body };
+  temp.id = comments.length + 1;
+  comments.push(temp);
+  res.send(comments);
+});
+
+// get all comments
+router.route("/").get((req, res, next) => {
+  res.json(comments);
+});
+
+// get specific comment
 router
   .route("/")
   // create
@@ -21,32 +36,19 @@ router
     temp.id = comments.length + 1;
     comments.push(temp);
     res.send(comments);
-  })
-  // read
-  .get((req, res, next) => {
-    res.json(comments);
-  })
-  // update
-  .put((req, res, next) => {
-    res.send(
-      `Method: ${req.method}\nStatus: ${res.statusCode}\nPath: ${req.baseUrl}`
-    );
   });
 
-router
-  .route("/:id")
-  // get specific comment
-  .get((req, res, next) => {
-    const comment = comments.find((comment) => comment.id == req.params.id);
-    !comment
-      ? (console.log("Comment not found"),
-        res.status(404).json({ error: "Comment not found" }))
-      : res.json(comment);
-  })
-  // delete
-  .delete((req, res, next) => {
-    comments = comments.filter((comment) => req.params.id != comment.id);
-    res.json(comments);
-  });
+// update comment
+router.route("/").put((req, res, next) => {
+  res.send(
+    `Method: ${req.method}\nStatus: ${res.statusCode}\nPath: ${req.baseUrl}`
+  );
+});
+
+// delete comment
+router.route("/:id").delete((req, res, next) => {
+  comments = comments.filter((comment) => req.params.id != comment.id);
+  res.json(comments);
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -12,38 +12,40 @@ router.use(routeLogger);
 router.use(express.json());
 
 // ROUTES
-router
-  .route("/")
-  // create
-  .post((req, res, next) => {
-    let temp = { ...req.body };
-    temp.id = users.length + 1;
-    users.push(temp);
-    res.send(users);
-  })
-  // read
-  .get((req, res) => {
-    res.json(users);
-  })
-  // update
-  .put((req, res) => {
-    res.send("Update Route");
-  });
 
-router
-  .route("/:id")
-  // get specific user
-  .get((req, res) => {
-    const user = users.find((user) => user.id == req.params.id);
-    !user
-      ? (console.log("User not found"),
-        res.status(404).json({ error: "User not found" }))
-      : res.json(user);
-  })
-  // delete
-  .delete((req, res, next) => {
-    users = users.filter((user) => req.params.id != user.id);
-    res.json(users);
-  });
+const base = router.route("/");
+
+// create user
+router.route("/").post((req, res, next) => {
+  let temp = { ...req.body };
+  temp.id = users.length + 1;
+  users.push(temp);
+  res.send(users);
+});
+
+// get all users
+router.route("/").get((req, res) => {
+  res.json(users);
+});
+
+// get specific user
+router.route("/:id").get((req, res) => {
+  const user = users.find((user) => user.id == req.params.id);
+  !user
+    ? (console.log("User not found"),
+      res.status(404).json({ error: "User not found" }))
+    : res.json(user);
+});
+
+// update user
+router.route("/").put((req, res) => {
+  res.send("Update Route");
+});
+
+// delete user
+router.route("/:id").delete((req, res, next) => {
+  users = users.filter((user) => req.params.id != user.id);
+  res.json(users);
+});
 
 module.exports = router;
